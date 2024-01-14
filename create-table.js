60ff61ce9b35fe0175f636a92f292ce88dcdfce5
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-01-14 04:01:34 -0300

    Criação API Node com Postgres

---
+++ create-table.js
@@ -0,0 +1,12 @@
+import { sql } from './db.js'
+
+//sql`DROP TABLE IF EXISTS videos`.then(() => console.log("Tabela Apagada"))
+
+sql`
+CREATE TABLE videos (
+id TEXT PRIMARY KEY,   
+title TEXT,
+description TEXT,
+duration INTEGER    
+);
+`.then(() => console.log("Tabela criada"))
---
+++ server.js
@@ -0,0 +1,52 @@
+import { fastify } from "fastify"
+import { DataBaseMemoryPostgres } from "./database-postgres.js"
+//import { DataBaseMemory } from "./database-memory.js"
+
+const server = fastify()
+const database = new DataBaseMemoryPostgres()
+
+server.post("/videos", async (request, response) => {
+    const { title, description, duration } = request.body
+
+    await database.create({
+        title: title,
+        description: description,
+        duration: duration,
+    })
+
+    console.log(database.list())
+
+    return response.status(201).send()
+})
+
+server.get("/list", async (request, response) => {
+    const search = request.query.search
+    const videos = await database.list(search)
+
+    return videos
+})
+
+server.put("/videos/:id", async (request, response) => {
+    const videoId = request.params.id
+    const { title, description, duration } = request.body
+
+    await database.update(videoId, {
+        title,
+        description,
+        duration
+    })
+
+    return response.status(204).send()
+})
+
+server.delete("/videos/:id", async (request, response) => {
+
+    const videoId = request.params.id
+    await database.delete(videoId)
+
+    return response.status(204).send()
+})
+
+server.listen({
+    port: process.env.PORT ?? 3333,
+})
